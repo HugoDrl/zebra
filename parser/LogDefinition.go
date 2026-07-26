1661f7acae599607eb874fb7523906e026b94908
Hugo <hugo.derlyn@gmail.com>
2026-07-27 00:04:04 +0200

    WIP: test parse lines

--- parser/LogDefinition.go
+++ parser/LogDefinition.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"maps"
 	"time"
 )
 
@@ -21,6 +22,16 @@ type Log struct {
 	Message  string
 	Service  string
 	extra    map[string]string
+}
+
+func (l Log) Equal(log Log) bool {
+	return (
+		l.Time.Equal(log.Time) &&
+		l.Level == log.Level &&
+		l.Duration == log.Duration &&
+		l.Message == log.Message &&
+		l.Service == log.Service &&
+		maps.Equal(l.extra, log.extra))
 }
 
 type ParseSettings struct {
---
+++ parser/parse_line_log_test.go
@@ -0,0 +1,52 @@
+package parser
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParseLineLog(t *testing.T) {
+	tests := map[string]struct{
+		input string
+		expected Log
+		expectedErr bool
+	}{
+		"standard log": {
+			input: "2026-01-01T00:00:00Z WARNING service=test message=hey duration=50ms",
+			expected: Log{
+				Time: time.Date(2026, 01, 01, 0, 0, 0, 0, time.UTC),
+				Level: Warning,
+				Service: "test",
+				Message: "hey",
+				Duration: 50 * time.Millisecond,
+			},
+		},
+		"invalid service": {
+			input: "2026-01-01T00:00:00Z INVALID_SERVICE service=test message=hey duration=50ms",
+			expectedErr: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			log, err := parseLine(test.input)
+			if (err != nil) != test.expectedErr {
+				t.Fatalf(
+					"%s: unexpected err behavior - got %t, expected %t (err: %s)",
+					name,
+					err != nil,
+					test.expectedErr,
+					err,
+				)
+			}
+			if !(*log).Equal(test.expected) {
+				t.Fatalf(
+					"%s: expected %s - got %s",
+					name,
+					test.expected,
+					log,
+				)
+			}
+		})
+	}
+}
