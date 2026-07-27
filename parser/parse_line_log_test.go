97ab7e2bc9cb49abdc9292dcb4d02e77067f31cd
Hugo <hugo.derlyn@gmail.com>
2026-07-27 11:06:32 +0200

    add tests for line parsing

--- parser/parse_line_log_test.go
+++ parser/parse_line_log_test.go
@@ -21,9 +21,33 @@ func TestParseLineLog(t *testing.T) {
 				Duration: 50 * time.Millisecond,
 			},
 		},
-		"invalid service": {
+		"invalid: invalid service": {
 			input: "2026-01-01T00:00:00Z INVALID_SERVICE service=test message=hey duration=50ms",
 			expectedErr: true,
+		},
+		"invalid: invalid date": {
+			input: "INVALID_DATE WARNING service=test message=hey duration=50ms",
+			expectedErr: true,
+		},
+		"invalid: invalid duration": {
+			// duration should (for now) only be in milliseconds
+			input: "2026-01-01T00:00:00Z WARNING service=test message=hey duration=50s",
+			expectedErr: true,
+		},
+		"invalid: missing field": {
+			// duration should (for now) only be in milliseconds
+			input: "2026-01-01T00:00:00Z WARNING service=test duration=50s",
+			expectedErr: true,
+		},
+		"log with sentence as message": {
+			input: "2026-01-01T00:00:00Z WARNING service=test message=\"this log is very important\" duration=50ms",
+			expected: Log{
+				Time: time.Date(2026, 01, 01, 0, 0, 0, 0, time.UTC),
+				Level: Warning,
+				Service: "test",
+				Message: "\"this log is very important\"",
+				Duration: 50 * time.Millisecond,
+			},
 		},
 	}
 
