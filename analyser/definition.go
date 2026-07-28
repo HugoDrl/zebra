3e40ff4004a7e5c4aded60c1b0ce8ef0819cc445
Hugo <hugo.derlyn@gmail.com>
2026-07-28 20:36:37 +0200

    feat: create tests for equality

--- analyser/definition.go
+++ analyser/definition.go
@@ -2,6 +2,8 @@ package analyser
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 	"time"
 
 	"github.com/HugoDrl/LogParser/parser"
@@ -45,6 +47,15 @@ func (m *CollectionMetric) Display() {
 			fmt.Println(e.Error())
 		}
 	}
+}
+
+func (m *CollectionMetric) IsEqual(metric *CollectionMetric) bool {
+	return (maps.Equal(m.Lines, metric.Lines) &&
+		maps.Equal(m.ServicePerformance, metric.ServicePerformance) &&
+		slices.Equal(m.FileErrors, metric.FileErrors) &&
+		m.ParsingErrorCount == metric.ParsingErrorCount &&
+		slices.Equal(m.SlowestInput, metric.SlowestInput) &&
+		m.Query == metric.Query)
 }
 
 type ServiceMetric struct {
---
+++ analyser/definition_test.go
@@ -0,0 +1,44 @@
+package analyser
+
+import (
+	"github.com/HugoDrl/LogParser/parser"
+	"testing"
+	"time"
+)
+
+func TestMetricsEquality(t *testing.T) {
+	metric := &CollectionMetric{
+		Lines: map[parser.Level]int{},
+		ServicePerformance: map[string]*ServiceMetric{
+			"database": {
+				Name:            "database",
+				Lines:           3,
+				AverageDuration: 50 * time.Millisecond,
+			},
+			"api": {
+				Name:            "api",
+				Lines:           1,
+				AverageDuration: 22 * time.Millisecond,
+			},
+		},
+	}
+	tests := map[string]struct {
+		input    *CollectionMetric
+		expected bool
+	}{
+		"same pointer should be equal": {
+			input:    metric,
+			expected: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			output := metric.IsEqual(test.input)
+
+			if output != test.expected {
+				t.Fatalf("%s: expected %t - got %t", name, test.expected, output)
+			}
+		})
+	}
+}
