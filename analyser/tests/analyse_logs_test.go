62e2c1dbfb589e297e2a65946eeb019dedd8410d
Hugo <hugo.derlyn@gmail.com>
2026-08-01 13:37:43 +0200

    fix: use black box testing for exported functions

--- analyser/tests/analyse_logs_test.go
+++ analyser/tests/analyse_logs_test.go
@@ -1,8 +1,9 @@
-package analyser
+package analyser_test
 
 import (
 	"testing"
 
+	"github.com/HugoDrl/LogParser/analyser"
 	"github.com/HugoDrl/LogParser/parser"
 	"github.com/google/go-cmp/cmp"
 )
@@ -17,35 +18,34 @@ func feedThenCloseChan[T any](values ...T) chan T {
 }
 
 func TestLogAnalyze(t *testing.T) {
-	type LogAnalyzeInput struct{
-			logChan chan *parser.Log
-			errChan chan error
-			settings *AnalyserSettings
-		}
+	type LogAnalyzeInput struct {
+		logChan  chan *parser.Log
+		errChan  chan error
+		settings *analyser.AnalyserSettings
+	}
 
-	tests := map[string]struct{
-		input LogAnalyzeInput
-		expected *CollectionMetric
+	tests := map[string]struct {
+		input    LogAnalyzeInput
+		expected *analyser.CollectionMetric
 	}{
 		"empty everything should result in empty metrics": {
 			input: LogAnalyzeInput{
 				logChan: feedThenCloseChan([]*parser.Log{}...),
 				errChan: feedThenCloseChan([]error{}...),
 			},
-			expected: &CollectionMetric{
-				Lines: map[parser.Level]int{},
-				ServicePerformance: map[string]ServiceMetric{},
+			expected: &analyser.CollectionMetric{
+				Lines:              map[parser.Level]int{},
+				ServicePerformance: map[string]analyser.ServiceMetric{},
 			},
 		},
 	}
 
 	for name, test := range tests {
-		t.Run(name, func(t *testing.T){
-			output := AnalyseLogs(test.input.logChan, test.input.errChan, test.input.settings)
+		t.Run(name, func(t *testing.T) {
+			output := analyser.AnalyseLogs(test.input.logChan, test.input.errChan, test.input.settings)
 			if diff := cmp.Diff(
 				test.expected,
 				output,
-				cmp.AllowUnexported(CollectionMetric{}),
 			); diff != "" {
 				t.Fatal(diff)
 			}
