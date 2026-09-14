5ff027c7e70da833118134e18a7c9d50a3a28e28
Hugo <hugo.derlyn@gmail.com>
2026-09-14 23:03:03 +0200

    feat/retrieve logs endpoint

--- internal/server/metrics_handler.go
+++ internal/server/metrics_handler.go
@@ -8,6 +8,18 @@ import (
 	"github.com/HugoDrl/zebra/internal/analyser"
 )
 
+func (d *DataLayer) retrieveLogs(w http.ResponseWriter, r *http.Request) {
+	payload, err := json.Marshal(d.Logs)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	if _, err := w.Write(payload); err != nil {
+		w.WriteHeader(500)
+	}
+}
+
 func (d *DataLayer) calculateMetrics(w http.ResponseWriter, r *http.Request) {
 	metrics := analyser.AnalyseLogs(d.Logs, d.Errs)
 
@@ -43,6 +55,7 @@ func (d *DataLayer) retrieveSlowestLogs(w http.ResponseWriter, r *http.Request) 
 }
 
 func (d *DataLayer) AttachMetricsHandler(handler *http.ServeMux) {
+	handler.HandleFunc("GET /logs", d.retrieveLogs)
 	handler.HandleFunc("GET /metrics", d.calculateMetrics)
 	handler.HandleFunc("GET /slowest_logs", d.retrieveSlowestLogs)
 }
